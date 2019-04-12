d35649df8f78c96ff8468bc74721e66bbf30f115
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-04-12 11:33:33 -0500

    Set up express and router usage

---
+++ server.js
@@ -0,0 +1,18 @@
+// Require Express
+var express = require("express");
+// Set up PORT to either designated port or 3000
+var PORT = process.env.PORT || 3000;
+// Make use of Express app
+var app = express();
+// Set up Express router
+var router = express.Router();
+//Designate public folder as static directory
+app.use(express.static(__dirname + "/public"));
+// Router middleware
+app.use(router);
+
+//Listen on PORT
+app.listen(PORT, function() {
+    console.log("Listening on port:" + PORT);
+});
+
