d227ba4f07fd439c2bab576547b7f1448f341d12
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-04-12 15:10:46 -0500

    Created the CommentSchema objects and exported it

---
+++ models/Comments.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var CommentSchema = new Schema ({
+    _ArticleId: {
+        type: Schema.Types.ObjectId,
+        ref: "Articles"
+    },
+    date: String,
+    commentText: {
+        type: String
+    }
+});
+
+//Set up the model and export Articles
+var Comment = mongoose.model("Comments", CommentSchema);
+module.exports = Comments;
