8621429a41c83f59a31b67515999dafd6d63c1ed
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-04-12 23:30:03 -0500

    Too much to list as there are so many things

--- server.js
+++ server.js
@@ -48,6 +48,53 @@ mongoose.connect(db, function (error) {
     }
 });
 
+app.get("/all", function (req, res) {
+    //this is supposed to retrieve all the data from the scrapedArticles collection..but I keep getting the  "find" ///undefined error
+    db.scrapedArticles.find({} , function (err, found) {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.json(found);
+        }
+    });
+});
+
+app.get("/scrape", function (req, res) {
+    axios.get("http://www.cnn.com/").then(function (response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
+
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("cd cd--card cd--article cd--idx-0 cd--large cd--vertical cd--has-siblings cd--has-media cd--media__image").each(function (i, element) {
+            var article = $(this).children("h3.cd__headline").text();
+            var headlineText = $(this).children("span.cd__headline-text").text();
+            var link = $(this).children("a").attr("href");
+
+            if (article && headlineText && result) {
+                db.scrapedArticles.save({
+                        article: article,
+                        headlineText: headlineText,
+                        link: link
+                    },
+
+                    function (error, save) {
+                        if (error) {
+                            console.log(error);
+                        } else {
+                            console.log(save);
+                        }
+                    });
+            }
+
+        });
+
+    });
+
+        res.send("Scrape complete!");
+
+});
+
 
 //Listen on PORT
 app.listen(PORT, function () {
