09a027d5de16e3eff8af52b43603be7d7027f596
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-04-12 23:29:09 -0500

    Started writing out the app.js

---
+++ public/assets/javascript/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+ $("#article-contents").empty();
+ $("#message").text("No new articles at this time!");
+
+ // When a user clicks on the New scrape button, 
+ // Needed to use a get route from the cnn website to scrape ...BUT that's already been complete in server.js 
+ //I wanted to have the div #article-contents show a modal or something say "Scrape complete" instead of having a get route for /scrape...it's in the server.js 
+ // Next I wanted to retrieve from my database the collected articles
+ 
+ 
+
+});
+  
+
+ 
