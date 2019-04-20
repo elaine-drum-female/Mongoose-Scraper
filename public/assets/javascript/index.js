2109e9763537f2f6f231bf43a22813b5335ffc38
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-04-19 22:41:37 -0500

    Provided the pseudocode

---
+++ public/assets/javascript/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    // I want to be able to click on the new scrape button and 
+    // articles-container would become empty
+    // then axios would get the website url to see if there are any saved articles
+    // if there are render them, else leave empty
+    
+    // Here you would want to append the article to the page
+    // By creating dynamic content like div containers, classes, p tag, etc...
+    // Somewhere pass on the data id from the article?
+    // return the new div
+
+    // Do something with the save articles
+
+    // Run a function to scrape new articles using fetch
+
+    // Handle the delete function and the comments
+});
